08d5d76597e91e39f2f5fab7f94c07a926cc4fa3
lwflwf1 <799413690@qq.com>
2026-07-29 21:59:02 +0800

    todo use fzf

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -3,7 +3,7 @@ return {
     event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-        { "<leader>tf", "<cmd>TodoTelescope<CR>", desc = "TODOs search in Telescope" },
+        { "<leader>tf", "<cmd>TodoFzfLua<CR>", desc = "TODOs search (fzf)" },
         { "<leader>tq", "<cmd>TodoQuickFix<CR>", desc = "TODOs to quickfix" },
         { "<leader>tn", "<cmd>lua require('todo-comments').jump_next()<CR>", desc = "Next TODO" },
         { "<leader>tp", "<cmd>lua require('todo-comments').jump_prev()<CR>", desc = "Prev TODO" },
