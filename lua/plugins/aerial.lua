4d95e32e21ae191acceb5294d73b548edb1bc209
lwflwf1 <799413690@qq.com>
2026-07-31 11:41:55 +0800

    update aerial

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -13,34 +13,131 @@ return {
             { "]s", function() require("aerial").next() end, desc = "Next symbol" },
             { "[s", function() require("aerial").prev() end, desc = "Prev symbol" },
         },
-        opts = {
-            attach_mode = "global",
-            manage_folds = true,
-            link_folds_to_tree = true,
-            link_tree_to_folds = true,
-            backends = { "treesitter", "lsp", "markdown" },
-            layout = {
-                default_direction = "prefer_right",
-                max_width = { 40, 0.2 },
-                min_width = 10,
-            },
-            filter_kind = {
-                "Class",
-                "Constructor",
-                "Enum",
-                "Function",
-                "Interface",
-                "Module",
-                "Method",
-                "Struct",
-                "Variable",
-                "Property",
-            },
-            show_guides = true,
-            on_attach = function(bufnr)
-                vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr, desc = "Previous symbol" })
-                vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Next symbol" })
-            end,
-        },
+        opts = function()
+            local function apply_filter(src_bufnr, kinds)
+                if kinds then
+                    vim.b[src_bufnr].aerial_filter_kind = kinds
+                else
+                    vim.b[src_bufnr].aerial_filter_kind = nil
+                end
+                require("aerial").refetch_symbols(src_bufnr)
+            end
+
+            local function src_bufnr()
+                return vim.api.nvim_buf_get_var(vim.api.nvim_get_current_buf(), "source_buffer")
+            end
+
+            local containers = {
+                systemverilog = { "Class", "Interface", "Module", "Package" },
+                python = { "Class", "Module" },
+                perl = { "Package" },
+                cpp = { "Class", "Struct", "Namespace" },
+                c = { "Struct", "Namespace" },
+                lua = { "Module", "Class" },
+                rust = { "Module", "Struct", "Enum", "Trait", "Impl" },
+                typescript = { "Class", "Interface", "Namespace", "Enum" },
+                javascript = { "Class", "Namespace" },
+                _ = { "Class", "Interface", "Module", "Package", "Namespace" },
+            }
+
+            local function container_kinds(bufnr)
+                return containers[vim.bo[bufnr or src_bufnr()].filetype] or containers["_"]
+            end
+
+            local function filter(kinds, desc)
+                return {
+                    callback = function()
+                        local sb = src_bufnr()
+                        local merged = kinds and vim.list_extend(vim.deepcopy(kinds), container_kinds(sb)) or nil
+                        apply_filter(sb, merged)
+                    end,
+                    desc = desc or "",
+                }
+            end
+
+            local function open_kind_picker(source_bufnr)
+                local items = {}
+                local kinds = { "Module", "Interface", "Package", "Class", "Constructor", "Function", "Property", "Constraint", "AssertProperty", "Clocking", "Block", "Covergroup", "Variable" }
+                for _, k in ipairs(kinds) do
+                    table.insert(items, { text = k, kind = k })
+                end
+                Snacks.picker.pick({
+                    items = items,
+                    title = "Aerial kind filter",
+                    layout = { preset = "select", style = "minimal" },
+                    format = "text",
+                    actions = {
+                        confirm = function(picker, item)
+                            picker:close()
+                            apply_filter(source_bufnr, item and vim.list_extend(vim.deepcopy(container_kinds(source_bufnr)), { item.kind }) or nil)
+                        end,
+                    },
+                })
+            end
+
+            return {
+                attach_mode = "global",
+                autojump = true,
+                highlight_on_hover = true,
+                highlight_on_jump = false,
+                nerd_font = true,
+                manage_folds = true,
+                link_folds_to_tree = true,
+                link_tree_to_folds = true,
+                backends = { "treesitter", "lsp", "markdown" },
+                layout = {
+                    default_direction = "prefer_right",
+                    max_width = { 40, 0.2 },
+                    min_width = 10,
+                },
+                filter_kind = {
+                    "Class", "Constructor", "Function", "Interface", "Module",
+                    "Variable", "Property", "Constraint", "AssertProperty",
+                    "Clocking", "Block", "Covergroup",
+                },
+                icons = {
+                    Clocking       = "",
+                    Block          = "󰦮",
+                    Covergroup     = "󰓾",
+                    AssertProperty = "",
+                    Constraint     = "󰗴",
+                },
+                show_guides = true,
+                keymaps = {
+                    ["sf"] = filter({ "Function" }, "Filter: functions"),
+                    ["sc"] = filter({ "Class", "Interface", "Module", "Package" }, "Filter: classes/modules"),
+                    ["sp"] = filter({ "Property", "Constraint", "AssertProperty" }, "Filter: properties"),
+                    ["sv"] = filter({ "Variable" }, "Filter: variables"),
+                    ["so"] = filter({ "Clocking", "Block", "Covergroup" }, "Filter: other"),
+                    ["sa"] = filter(nil, "Show all symbols"),
+                    ["ss"] = {
+                        callback = function()
+                            open_kind_picker(src_bufnr())
+                        end,
+                        desc = "Select kinds",
+                    },
+                },
+            }
+        end,
+        config = function(_, opts)
+            vim.lsp.protocol.SymbolKind.Constraint = 27
+            vim.lsp.protocol.SymbolKind.Clocking = 28
+            vim.lsp.protocol.SymbolKind.Block = 29
+            vim.lsp.protocol.SymbolKind.Covergroup = 30
+            vim.lsp.protocol.SymbolKind.AssertProperty = 31
+            require("aerial").setup(opts)
+            vim.api.nvim_set_hl(0, "AerialLine", { link = "IncSearch", default = true })
+
+            -- Custom kind highlights
+            vim.api.nvim_set_hl(0, "AerialClockingIcon",       { link = "Special",    default = true })
+            vim.api.nvim_set_hl(0, "AerialBlockIcon",          { link = "Special",    default = true })
+            vim.api.nvim_set_hl(0, "AerialCovergroupIcon",     { link = "Special",    default = true })
+            vim.api.nvim_set_hl(0, "AerialAssertPropertyIcon", { link = "Special",    default = true })
+            vim.api.nvim_set_hl(0, "AerialConstraintIcon",     { link = "Special",    default = true })
+            vim.api.nvim_set_hl(0, "AerialClocking",            { link = "AerialNormal", default = true })
+            vim.api.nvim_set_hl(0, "AerialBlock",               { link = "AerialNormal", default = true })
+            vim.api.nvim_set_hl(0, "AerialCovergroup",          { link = "AerialNormal", default = true })
+            vim.api.nvim_set_hl(0, "AerialAssertProperty",      { link = "AerialNormal", default = true })
+        end,
     },
 }
---
+++ queries/systemverilog/aerial.scm
@@ -0,0 +1,131 @@
+;-------------------------------------
+; Module / Interface / Package
+;-------------------------------------
+(module_declaration
+  (module_ansi_header name: (simple_identifier) @name)
+  (#set! "kind" "Module")) @type
+
+(module_declaration
+  (module_nonansi_header name: (simple_identifier) @name)
+  (#set! "kind" "Module")) @type
+
+(module_declaration
+  .
+  (simple_identifier) @name
+  (#set! "kind" "Module")) @type
+
+(interface_declaration
+  (interface_ansi_header name: (simple_identifier) @name)
+  (#set! "kind" "Interface")) @type
+
+(interface_declaration
+  (interface_nonansi_header name: (simple_identifier) @name)
+  (#set! "kind" "Interface")) @type
+
+(interface_declaration
+  .
+  (simple_identifier) @name
+  (#set! "kind" "Interface")) @type
+
+(package_declaration name: (simple_identifier) @name
+  (#set! "kind" "Package")) @type
+
+;-------------------------------------
+; Clocking
+;-------------------------------------
+(clocking_declaration name: (simple_identifier) @name
+  (#set! "kind" "Clocking")) @type
+
+(clocking_declaration !name
+  (#set! "kind" "Clocking")) @type
+
+;-------------------------------------
+; Generate
+;-------------------------------------
+(generate_block name: (simple_identifier) @name
+  (#set! "kind" "Block")) @type
+
+(generate_block !name
+  (#set! "kind" "Block")) @type
+
+;-------------------------------------
+; Parallel block (fork/join)
+;-------------------------------------
+(par_block
+  .
+  (simple_identifier)? @name
+  (#set! "kind" "Block")) @type
+
+;-------------------------------------
+; Class / Constructor / Property
+;-------------------------------------
+(class_declaration name: (simple_identifier) @name
+  (#set! "kind" "Class")) @type
+
+(class_constructor_declaration "new" @name
+  (#set! "kind" "Constructor")) @type
+
+(class_property
+  (data_declaration
+    (list_of_variable_decl_assignments
+      (variable_decl_assignment
+        name: (simple_identifier) @name)))
+  (#set! "kind" "Property")) @type
+
+;-------------------------------------
+; Interface class / Covergroup / Assertion
+;-------------------------------------
+(interface_class_declaration name: (simple_identifier) @name
+  (#set! "kind" "Interface")) @type
+
+(covergroup_declaration name: (simple_identifier) @name
+  (#set! "kind" "Covergroup")) @type
+
+(property_declaration name: (simple_identifier) @name
+  (#set! "kind" "AssertProperty")) @type
+
+;-------------------------------------
+; Variable / Net declarations
+;-------------------------------------
+(data_declaration
+  (list_of_variable_decl_assignments
+    (variable_decl_assignment
+      name: (simple_identifier) @name))
+  (#not-has-ancestor? @type class_property)
+  (#set! "kind" "Variable")) @type
+
+(net_declaration
+  (list_of_net_decl_assignments
+    (net_decl_assignment
+      (simple_identifier) @name))
+  (#not-has-ancestor? @type class_property)
+  (#set! "kind" "Variable")) @type
+
+;-------------------------------------
+; Function / Task
+;-------------------------------------
+(function_declaration
+  (function_body_declaration
+    name: (simple_identifier) @name)
+  (#set! "kind" "Function")) @type
+
+(function_prototype name: (simple_identifier) @name
+  (#set! "kind" "Function")) @type
+
+(task_declaration
+  (task_body_declaration
+    name: (simple_identifier) @name)
+  (#set! "kind" "Function")) @type
+
+(task_prototype name: (simple_identifier) @name
+  (#set! "kind" "Function")) @type
+
+;-------------------------------------
+; Constraint
+;-------------------------------------
+(constraint_declaration
+  . (simple_identifier) @name
+  (#set! "kind" "Constraint")) @type
+
+(constraint_prototype (simple_identifier) @name
+  (#set! "kind" "Constraint")) @type
