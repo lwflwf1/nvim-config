5f7996e10d1157c87d08264691fe8fc8991ecc51
lwflwf1 <799413690@qq.com>
2026-08-21 20:37:34 +0800

    feat(grug-far): follow symlinks (--follow) + fix jj in its buffer
    - engines.ripgrep.extraArgs = '--follow' (searches AND replacements)
    - mini.keymap's jj combo (nvim_input replay of <BS><BS><Esc>) misfires in the
      grug-far buffer (jj stays inserted); disable it there (vim.b.minikeymap_disable)
      and use a clean buffer-local i: jj -> <Esc>

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -6,6 +6,25 @@ return {
       { "<leader>fx", "<cmd>GrugFar<cr>", mode = {"n"}, desc = "Find and Replace" },
       { "<leader>fx", "<cmd>GrugFarWithin<cr>", mode = {"v"}, desc = "Find and Replace" },
     },
-    opts = {},
+    opts = {
+      -- follow symlinks in searches/replacements (the box tree is full of links)
+      engines = {
+        ripgrep = {
+          extraArgs = '--follow',
+        },
+      },
+    },
+    config = function()
+      -- mini.keymap's `jj` combo replays <BS><BS><Esc> via nvim_input, which
+      -- misfires in the grug-far buffer (jj stays inserted, insert mode exits).
+      -- Disable the combo machinery here and use a clean buffer-local mapping.
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "grug-far",
+        callback = function()
+          vim.b.minikeymap_disable = true
+          vim.keymap.set("i", "jj", "<Esc>", { buffer = true })
+        end,
+      })
+    end,
   },
 }
